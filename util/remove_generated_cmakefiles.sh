f0b161b747721e28ea9485985074e95ea510f79d
Andreas Zahnleiter <mrcerealguy@gmx.de>
2016-11-29 13:03:16 +0100

    Add files via upload

---
+++ util/remove_generated_cmakefiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..
+
+find . -type f -name "Makefile" -exec rm {} \;
+# find . -type f -name "CMakeCache.txt" -exec rm {} \;
+find . -type f -name "cmake_install.cmake" -exec rm {} \;
+find . -type f -name "CPackConfig.cmake" -exec rm {} \;
+find . -type f -name "CPackSourceConfig.cmake" -exec rm {} \;
+find . -type d -name CMakeFiles -exec rm -r {} \;
---
+++ util/remove_tmpfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ..
+find . -type f \( -name "*.bak" -o -name "*.dmp" \) -delete
