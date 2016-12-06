4c6f01a0820f46ab54eb41cfef015aea938c98ed
Andreas Zahnleiter <mrcerealguy@gmx.de>
2016-12-06 12:38:08 +0100

    Add files via upload

--- util/remove_generated_cmakefiles.sh
+++ util/remove_generated_cmakefiles.sh
@@ -2,9 +2,11 @@
 
 cd ..
 
-find . -type f -name "Makefile" -exec rm {} \;
-find . -type f -name "CMakeCache.txt" -exec rm {} \;
-find . -type f -name "cmake_install.cmake" -exec rm {} \;
-find . -type f -name "CPackConfig.cmake" -exec rm {} \;
-find . -type f -name "CPackSourceConfig.cmake" -exec rm {} \;
-find . -type d -name CMakeFiles -exec rm -r {} \;
+echo -e "\E[34;47mRemove previous CMake files..."
+find . -type f -name "Makefile" -exec rm -vf {} \;
+find . -type f -name "CMakeCache.txt" -exec rm -vf {} \;
+find . -type f -name "cmake_install.cmake" -exec rm -vf {} \;
+find . -type f -name "CPackConfig.cmake" -exec rm -vf {} \;
+find . -type f -name "CPackSourceConfig.cmake" -exec rm -vf {} \;
+find . -type d -name CMakeFiles -exec rm -rvf {} \;
+echo -e "\E[34;47mdone!"
