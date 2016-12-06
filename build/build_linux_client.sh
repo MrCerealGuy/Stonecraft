bdb2e937d93be87e2306ac77bc0daffaba8ccc2d
Andreas Zahnleiter <mrcerealguy@gmx.de>
2016-12-06 15:43:35 +0100

    Add files via upload

--- build/build_linux_client.sh
+++ build/build_linux_client.sh
@@ -1,5 +1,23 @@
+dialog --backtitle "Stonecraft Build" --title "Stonecraft Build" --yesno "Start the build process? All previous generated CMake files will be deleted!" 15 60
+antwort=${?}
+
+if [ "$antwort" -eq "255" ]
+  then
+    echo "Aborted"
+    exit 255
+fi
+
+if [ "$antwort" -eq "1" ]
+  then
+    exit 1
+fi
+
+if [ "$antwort" -eq "0" ]
+  then
+    ../util/remove_generated_cmakefiles.sh
+fi
+
 cd ../
 cmake . -DBUILD_CLIENT=1 -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=0 -DENABLE_REDIS=0
-#cmake . -DBUILD_CLIENT=1 -DENABLE_GETTEXT=1 -DENABLE_FREETYPE=1 -DENABLE_LEVELDB=1 -DENABLE_REDIS=1
 make -j$(grep -c processor /proc/cpuinfo)
  
--- build/build_linux_server.sh
+++ build/build_linux_server.sh
@@ -1,4 +1,23 @@
+dialog --backtitle "Stonecraft Build" --title "Stonecraft Build" --yesno "Start the build process? All previous generated CMake files will be deleted!" 15 60
+antwort=${?}
+
+if [ "$antwort" -eq "255" ]
+  then
+    echo "Aborted"
+    exit 255
+fi
+
+if [ "$antwort" -eq "1" ]
+  then
+    exit 1
+fi
+
+if [ "$antwort" -eq "0" ]
+  then
+    ../util/remove_generated_cmakefiles.sh
+fi
+
 cd ../
-cmake . -DBUILD_CLIENT=0 -DIRRLICHT_SOURCE_DIR=../libraries/irrlicht-1.8.3/source/Irrlicht/
+cmake . -DBUILD_CLIENT=0 -DIRRLICHT_SOURCE_DIR=./libs/msvc2013-win-i686/irrlicht-1.8.3/source/Irrlicht/
 make -j$(grep -c processor /proc/cpuinfo)
  
