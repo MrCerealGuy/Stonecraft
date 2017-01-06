2a6cf5a071aa40e913086922619fdb0daad7e3e9
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-06 23:20:17 +0100

    Remove subgame stonecraft

--- games/stonecraft_game/mods/technic/technic/machines/other/coal_furnace.lua
+++
@@ -1,5 +0,0 @@
-local S = technic.getter
-
-if minetest.registered_nodes["default:furnace"].description == "Furnace" then
-	minetest.override_item("default:furnace", { description = S("Fuel-Fired Furnace") })
-end
