d101a2ce387a91066bb7d7af54cdac7ea58dc2b1
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-12 00:15:12 +0100

    Do not delete Makefile

--- util/remove_generated_cmakefiles.sh
+++ util/remove_generated_cmakefiles.sh
@@ -3,7 +3,7 @@
 cd ..
 
 echo -e "\E[34;47mRemove previous CMake files..."
-find . -type f -name "Makefile" -exec rm -vf {} \;
+#find . -type f -name "Makefile" -exec rm -vf {} \;
 find . -type f -name "CMakeCache.txt" -exec rm -vf {} \;
 find . -type f -name "cmake_install.cmake" -exec rm -vf {} \;
 find . -type f -name "CPackConfig.cmake" -exec rm -vf {} \;
