a51a0021a992cddcaca8391bc8c4252eb893a4a2
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-16 10:07:23 +0100

    Update mod mg_villages

--- games/stonecraft_game/mods/mg_villages/init.lua
+++ games/stonecraft_game/mods/mg_villages/init.lua
@@ -1,21 +1,3 @@
---[[
-
-2017-01-06 modified by MrCerealGuy <mrcerealguy@gmx.de>
-	exit if mod is deactivated
-
---]]
-
-local DIR_DELIM = DIR_DELIM or "/"
-local world_file = minetest.get_worldpath()..DIR_DELIM.."world.mt"
-local world_conf = Settings(world_file)
-local enable_villages = world_conf:get("enable_villages")
-
-if enable_villages ~= nil and enable_villages == "false" then
-	minetest.log("info", "[mg_villages] skip loading mod.")
-	return
-end
-
--- --------------------------------------------------------------------------------------------------------
 
 -- reserve namespace for the villages
 mg_villages = {}
@@ -91,4 +73,3 @@ dofile(mg_villages.modpath.."/highlandpools.lua")
 dofile(mg_villages.modpath.."/mapgen.lua")
 
 dofile(mg_villages.modpath.."/spawn_player.lua")
-
