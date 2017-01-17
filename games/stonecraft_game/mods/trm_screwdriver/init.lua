9638080b2400be1f53ecd759756bace51f44407f
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-17 10:41:00 +0100

    Added mod trm_screwdriver

---
+++ games/stonecraft_game/mods/trm_screwdriver/init.lua
@@ -0,0 +1,20 @@
+--[[
+
+2017-01-16 modified by MrCerealGuy <mrcerealguy@gmx.de>
+	exit if mod is deactivated
+
+--]]
+
+local DIR_DELIM = DIR_DELIM or "/"
+local world_file = minetest.get_worldpath()..DIR_DELIM.."world.mt"
+local world_conf = Settings(world_file)
+local enable_surprise = world_conf:get("enable_surprise")
+
+if enable_surprise ~= nil and enable_surprise == "false" then
+	minetest.log("info", "[trm_screwdriver] skip loading mod.")
+	return
+end
+
+-- --------------------------------------------------------------------------------------------------------
+
+treasurer.register_treasure("screwdriver:screwdriver",0.02,3,{1,2},nil,"tool")
