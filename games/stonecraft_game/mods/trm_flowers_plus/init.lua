0b55b302d3b2f4f3f1d636f42b35a3ea30875bee
MrCerealGuy <mrcerealguy@gmx.de>
2017-01-17 10:40:29 +0100

    Added mod trm_flowers_plus

---
+++ games/stonecraft_game/mods/trm_flowers_plus/init.lua
@@ -0,0 +1,20 @@
+--[[
+
+2017-01-16 modified by MrCerealGuy <mrcerealguy@gmx.de>
+	exit if mod is deactivated
+
+--]]
+
+local DIR_DELIM = DIR_DELIM or "/"
+local world_file = minetest.get_worldpath()..DIR_DELIM.."world.mt"
+local world_conf = Settings(world_file)
+local enable_surprise = world_conf:get("enable_surprise")
+
+if enable_surprise ~= nil and enable_surprise == "false" then
+	minetest.log("info", "[trm_flowers_plus] skip loading mod.")
+	return
+end
+
+-- --------------------------------------------------------------------------------------------------------
+
+treasurer.register_treasure("flowers:flower_pot",0.1,2.5,{1,5})
