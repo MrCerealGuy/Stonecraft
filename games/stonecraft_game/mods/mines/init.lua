6706186285603064526b255798d58e5639e7932f
MrCerealGuy <mrcerealguy@gmx.de>
2017-02-06 10:41:34 +0100

    Added enable_mines

--- games/stonecraft_game/mods/mines/init.lua
+++ games/stonecraft_game/mods/mines/init.lua
@@ -1,3 +1,23 @@
+--[[
+
+2017-02-05 modified by MrCerealGuy <mrcerealguy@gmx.de>
+	exit if mod is deactivated
+
+--]]
+
+local DIR_DELIM = DIR_DELIM or "/"
+local world_file = minetest.get_worldpath()..DIR_DELIM.."world.mt"
+local world_conf = Settings(world_file)
+local enable_mines = world_conf:get("enable_mines")
+
+if enable_mines ~= nil and enable_mines == "false" then
+	minetest.log("info", "[mines] skip loading mod.")
+	return
+end
+
+-- --------------------------------------------------------------------------------------------------------
+
+
 local MINE_DEEP_MIN = tonumber(minetest.setting_get("mines_deep_min"))
 local MINE_DEEP_MAX = tonumber(minetest.setting_get("mines_deep_max"))
 local MINE_FACTOR = tonumber(minetest.setting_get("mines_spawnfactor"))
