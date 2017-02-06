0296e9630ffa32487042c33cf3fd75476c35c599
MrCerealGuy <mrcerealguy@gmx.de>
2017-02-06 10:41:11 +0100

    Added enable_villages

--- games/stonecraft_game/mods/mg_villages/init.lua
+++ games/stonecraft_game/mods/mg_villages/init.lua
@@ -1,3 +1,22 @@
+--[[
+
+2017-02-05 modified by MrCerealGuy <mrcerealguy@gmx.de>
+	exit if mod is deactivated
+
+--]]
+
+local DIR_DELIM = DIR_DELIM or "/"
+local world_file = minetest.get_worldpath()..DIR_DELIM.."world.mt"
+local world_conf = Settings(world_file)
+local enable_villages = world_conf:get("enable_villages")
+
+if enable_villages ~= nil and enable_villages == "false" then
+	minetest.log("info", "[mg_villages] skip loading mod.")
+	return
+end
+
+-- --------------------------------------------------------------------------------------------------------
+
 
 -- reserve namespace for the villages
 mg_villages = {}
