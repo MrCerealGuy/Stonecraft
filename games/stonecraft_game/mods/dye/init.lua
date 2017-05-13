685601c2cebc34761c266af5690fa7a76a788691
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-13 12:31:48 +0200

    Added localization to dye mod

--- games/stonecraft_game/mods/dye/init.lua
+++ games/stonecraft_game/mods/dye/init.lua
@@ -1,5 +1,16 @@
 -- Other mods can use these for looping through available colors
 
+--[[
+
+2017-05-13 added intllib support
+
+--]]
+
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 dye = {}
 dye.basecolors = {"white", "grey", "black", "red", "yellow", "green", "cyan", "blue", "magenta"}
 dye.excolors = {"white", "lightgrey", "grey", "darkgrey", "black", "red", "orange", "yellow",
@@ -8,41 +19,41 @@ dye.excolors = {"white", "lightgrey", "grey", "darkgrey", "black", "red", "orang
 -- Make dye names and descriptions available globally
 
 dye.dyes = {
-	{"white",      "White"},
-	{"grey",       "Grey"},
-	{"dark_grey",  "Dark grey"},
-	{"black",      "Black"},
-	{"violet",     "Violet"},
-	{"blue",       "Blue"},
-	{"cyan",       "Cyan"},
-	{"dark_green", "Dark green"},
-	{"green",      "Green"},
-	{"yellow",     "Yellow"},
-	{"brown",      "Brown"},
-	{"orange",     "Orange"},
-	{"red",        "Red"},
-	{"magenta",    "Magenta"},
-	{"pink",       "Pink"},
+	{"white",      S("White")},
+	{"grey",       S("Grey")},
+	{"dark_grey",  S("Dark grey")},
+	{"black",      S("Black")},
+	{"violet",     S("Violet")},
+	{"blue",       S("Blue")},
+	{"cyan",       S("Cyan")},
+	{"dark_green", S("Dark green")},
+	{"green",      S("Green")},
+	{"yellow",     S("Yellow")},
+	{"brown",      S("Brown")},
+	{"orange",     S("Orange")},
+	{"red",        S("Red")},
+	{"magenta",    S("Magenta")},
+	{"pink",       S("Pink")},
 }
 
 -- This collection of colors is partly a historic thing, partly something else
 
 local dyes = {
-	{"white",      "White dye",      {dye=1, basecolor_white=1,   excolor_white=1,      unicolor_white=1}},
-	{"grey",       "Grey dye",       {dye=1, basecolor_grey=1,    excolor_grey=1,       unicolor_grey=1}},
-	{"dark_grey",  "Dark grey dye",  {dye=1, basecolor_grey=1,    excolor_darkgrey=1,   unicolor_darkgrey=1}},
-	{"black",      "Black dye",      {dye=1, basecolor_black=1,   excolor_black=1,      unicolor_black=1}},
-	{"violet",     "Violet dye",     {dye=1, basecolor_magenta=1, excolor_violet=1,     unicolor_violet=1}},
-	{"blue",       "Blue dye",       {dye=1, basecolor_blue=1,    excolor_blue=1,       unicolor_blue=1}},
-	{"cyan",       "Cyan dye",       {dye=1, basecolor_cyan=1,    excolor_cyan=1,       unicolor_cyan=1}},
-	{"dark_green", "Dark green dye", {dye=1, basecolor_green=1,   excolor_green=1,      unicolor_dark_green=1}},
-	{"green",      "Green dye",      {dye=1, basecolor_green=1,   excolor_green=1,      unicolor_green=1}},
-	{"yellow",     "Yellow dye",     {dye=1, basecolor_yellow=1,  excolor_yellow=1,     unicolor_yellow=1}},
-	{"brown",      "Brown dye",      {dye=1, basecolor_brown=1,   excolor_orange=1,     unicolor_dark_orange=1}},
-	{"orange",     "Orange dye",     {dye=1, basecolor_orange=1,  excolor_orange=1,     unicolor_orange=1}},
-	{"red",        "Red dye",        {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_red=1}},
-	{"magenta",    "Magenta dye",    {dye=1, basecolor_magenta=1, excolor_red_violet=1, unicolor_red_violet=1}},
-	{"pink",       "Pink dye",       {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_light_red=1}},
+	{"white",      S("White dye"),      {dye=1, basecolor_white=1,   excolor_white=1,      unicolor_white=1}},
+	{"grey",       S("Grey dye"),       {dye=1, basecolor_grey=1,    excolor_grey=1,       unicolor_grey=1}},
+	{"dark_grey",  S("Dark grey dye"),  {dye=1, basecolor_grey=1,    excolor_darkgrey=1,   unicolor_darkgrey=1}},
+	{"black",      S("Black dye"),      {dye=1, basecolor_black=1,   excolor_black=1,      unicolor_black=1}},
+	{"violet",     S("Violet dye"),     {dye=1, basecolor_magenta=1, excolor_violet=1,     unicolor_violet=1}},
+	{"blue",       S("Blue dye"),       {dye=1, basecolor_blue=1,    excolor_blue=1,       unicolor_blue=1}},
+	{"cyan",       S("Cyan dye"),       {dye=1, basecolor_cyan=1,    excolor_cyan=1,       unicolor_cyan=1}},
+	{"dark_green", S("Dark green dye"), {dye=1, basecolor_green=1,   excolor_green=1,      unicolor_dark_green=1}},
+	{"green",      S("Green dye"),      {dye=1, basecolor_green=1,   excolor_green=1,      unicolor_green=1}},
+	{"yellow",     S("Yellow dye"),     {dye=1, basecolor_yellow=1,  excolor_yellow=1,     unicolor_yellow=1}},
+	{"brown",      S("Brown dye"),      {dye=1, basecolor_brown=1,   excolor_orange=1,     unicolor_dark_orange=1}},
+	{"orange",     S("Orange dye"),     {dye=1, basecolor_orange=1,  excolor_orange=1,     unicolor_orange=1}},
+	{"red",        S("Red dye"),        {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_red=1}},
+	{"magenta",    S("Magenta dye"),    {dye=1, basecolor_magenta=1, excolor_red_violet=1, unicolor_red_violet=1}},
+	{"pink",       S("Pink dye"),       {dye=1, basecolor_red=1,     excolor_red=1,        unicolor_light_red=1}},
 }
 
 -- Define items
