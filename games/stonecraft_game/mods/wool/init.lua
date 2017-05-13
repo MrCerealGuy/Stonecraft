b8b076ea7507719c482115f7d7ae508cb0a09719
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-13 12:37:31 +0200

    Added localization to wool mod

--- games/stonecraft_game/mods/wool/init.lua
+++ games/stonecraft_game/mods/wool/init.lua
@@ -2,29 +2,40 @@
 -- colors, and then some recipes using more specific colors for a few non-base
 -- colors available. When crafting, the last recipes will be checked first.
 
+--[[
+
+2017-05-13 added intllib support
+
+--]]
+
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 local dyes = {
-	{"white",      "White",      "basecolor_white"},
-	{"grey",       "Grey",       "basecolor_grey"},
-	{"black",      "Black",      "basecolor_black"},
-	{"red",        "Red",        "basecolor_red"},
-	{"yellow",     "Yellow",     "basecolor_yellow"},
-	{"green",      "Green",      "basecolor_green"},
-	{"cyan",       "Cyan",       "basecolor_cyan"},
-	{"blue",       "Blue",       "basecolor_blue"},
-	{"magenta",    "Magenta",    "basecolor_magenta"},
-	{"orange",     "Orange",     "excolor_orange"},
-	{"violet",     "Violet",     "excolor_violet"},
-	{"brown",      "Brown",      "unicolor_dark_orange"},
-	{"pink",       "Pink",       "unicolor_light_red"},
-	{"dark_grey",  "Dark Grey",  "unicolor_darkgrey"},
-	{"dark_green", "Dark Green", "unicolor_dark_green"},
+	{"white",      S("White"),      "basecolor_white"},
+	{"grey",       S("Grey"),       "basecolor_grey"},
+	{"black",      S("Black"),      "basecolor_black"},
+	{"red",        S("Red"),        "basecolor_red"},
+	{"yellow",     S("Yellow"),     "basecolor_yellow"},
+	{"green",      S("Green"),      "basecolor_green"},
+	{"cyan",       S("Cyan"),       "basecolor_cyan"},
+	{"blue",       S("Blue"),       "basecolor_blue"},
+	{"magenta",    S("Magenta"),    "basecolor_magenta"},
+	{"orange",     S("Orange"),     "excolor_orange"},
+	{"violet",     S("Violet"),     "excolor_violet"},
+	{"brown",      S("Brown"),      "unicolor_dark_orange"},
+	{"pink",       S("Pink"),       "unicolor_light_red"},
+	{"dark_grey",  S("Dark Grey"),  "unicolor_darkgrey"},
+	{"dark_green", S("Dark Green"), "unicolor_dark_green"},
 }
 
 for i = 1, #dyes do
 	local name, desc, craft_color_group = unpack(dyes[i])
 
 	minetest.register_node("wool:" .. name, {
-		description = desc .. " Wool",
+		description = desc .. S(" Wool"),
 		tiles = {"wool_" .. name .. ".png"},
 		is_ground_content = false,
 		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
