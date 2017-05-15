dce2e74f900fc4fed2af4bc98aa36aa7c2ccb650
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-15 13:37:51 +0200

    Added localization to caverealms mod

--- games/stonecraft_game/mods/caverealms/crafting.lua
+++ games/stonecraft_game/mods/caverealms/crafting.lua
@@ -1,5 +1,16 @@
 --CaveRealms crafting.lua
 
+--[[
+
+2017-05-15 MrCerealGuy: added intllib support
+
+--]]
+
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 --CRAFT ITEMS--
 
 --mycena powder
