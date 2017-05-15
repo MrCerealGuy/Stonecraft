6284abeb9948aa850ded80f16a9783e5ad720fe9
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-15 15:56:38 +0200

    Added localization to 3d_armor/shields mod

--- games/stonecraft_game/mods/3d_armor/shields/init.lua
+++ games/stonecraft_game/mods/3d_armor/shields/init.lua
@@ -1,9 +1,20 @@
-local use_moreores = minetest.get_modpath("moreores")
+--[[
+
+2017-05-15 MrCerealGuy: added intllib support
+
+--]]
+
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
+--local use_moreores = minetest.get_modpath("moreores")
 
 -- Regisiter Shields
 
 minetest.register_tool("shields:shield_admin", {
-	description = "Admin Shield",
+	description = S("Admin Shield"),
 	inventory_image = "shields_inv_shield_admin.png",
 	groups = {armor_shield=1000, armor_heal=100, armor_use=0, not_in_creative_inventory=1},
 	wear = 0,
@@ -11,13 +22,13 @@ minetest.register_tool("shields:shield_admin", {
 
 if ARMOR_MATERIALS.wood then
 	minetest.register_tool("shields:shield_wood", {
-		description = "Wooden Shield",
+		description = S("Wooden Shield"),
 		inventory_image = "shields_inv_shield_wood.png",
 		groups = {armor_shield=5, armor_heal=0, armor_use=2000},
 		wear = 0,
 	})
 	minetest.register_tool("shields:shield_enhanced_wood", {
-		description = "Enhanced Wood Shield",
+		description = S("Enhanced Wood Shield"),
 		inventory_image = "shields_inv_shield_enhanced_wood.png",
 		groups = {armor_shield=8, armor_heal=0, armor_use=1000},
 		wear = 0,
@@ -34,13 +45,13 @@ end
 
 if ARMOR_MATERIALS.cactus then
 	minetest.register_tool("shields:shield_cactus", {
-		description = "Cactus Shield",
+		description = S("Cactus Shield"),
 		inventory_image = "shields_inv_shield_cactus.png",
 		groups = {armor_shield=5, armor_heal=0, armor_use=2000},
 		wear = 0,
 	})
 	minetest.register_tool("shields:shield_enhanced_cactus", {
-		description = "Enhanced Cactus Shield",
+		description = S("Enhanced Cactus Shield"),
 		inventory_image = "shields_inv_shield_enhanced_cactus.png",
 		groups = {armor_shield=8, armor_heal=0, armor_use=1000},
 		wear = 0,
@@ -57,7 +68,7 @@ end
 
 if ARMOR_MATERIALS.steel then
 	minetest.register_tool("shields:shield_steel", {
-		description = "Steel Shield",
+		description = S("Steel Shield"),
 		inventory_image = "shields_inv_shield_steel.png",
 		groups = {armor_shield=10, armor_heal=0, armor_use=500},
 		wear = 0,
@@ -66,7 +77,7 @@ end
 
 if ARMOR_MATERIALS.bronze then
 	minetest.register_tool("shields:shield_bronze", {
-		description = "Bronze Shield",
+		description = S("Bronze Shield"),
 		inventory_image = "shields_inv_shield_bronze.png",
 		groups = {armor_shield=10, armor_heal=6, armor_use=250},
 		wear = 0,
@@ -75,7 +86,7 @@ end
 
 if ARMOR_MATERIALS.diamond then
 	minetest.register_tool("shields:shield_diamond", {
-		description = "Diamond Shield",
+		description = S("Diamond Shield"),
 		inventory_image = "shields_inv_shield_diamond.png",
 		groups = {armor_shield=15, armor_heal=12, armor_use=100},
 		wear = 0,
@@ -84,7 +95,7 @@ end
 
 if ARMOR_MATERIALS.gold then
 	minetest.register_tool("shields:shield_gold", {
-		description = "Gold Shield",
+		description = S("Gold Shield"),
 		inventory_image = "shields_inv_shield_gold.png",
 		groups = {armor_shield=10, armor_heal=6, armor_use=250},
 		wear = 0,
@@ -93,7 +104,7 @@ end
 
 if ARMOR_MATERIALS.mithril then
 	minetest.register_tool("shields:shield_mithril", {
-		description = "Mithril Shield",
+		description = S("Mithril Shield"),
 		inventory_image = "shields_inv_shield_mithril.png",
 		groups = {armor_shield=15, armor_heal=12, armor_use=50},
 		wear = 0,
@@ -102,7 +113,7 @@ end
 
 if ARMOR_MATERIALS.crystal then
 	minetest.register_tool("shields:shield_crystal", {
-		description = "Crystal Shield",
+		description = S("Crystal Shield"),
 		inventory_image = "shields_inv_shield_crystal.png",
 		groups = {armor_shield=15, armor_heal=12, armor_use=50, armor_fire=1},
 		wear = 0,
