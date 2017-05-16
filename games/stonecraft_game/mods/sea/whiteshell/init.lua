1f656b37939ed9408b04c461de84183280b39e68
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-16 12:23:22 +0200

    Added localization to sea/whiteshell mod

--- games/stonecraft_game/mods/sea/whiteshell/init.lua
+++ games/stonecraft_game/mods/sea/whiteshell/init.lua
@@ -3,6 +3,8 @@
 2017-01-06 modified by MrCerealGuy <mrcerealguy@gmx.de>
 	exit if mod is deactivated
 
+2017-05-16 MrCerealGuy: added intllib support
+
 --]]
 
 local DIR_DELIM = DIR_DELIM or "/"
@@ -17,11 +19,14 @@ end
 
 -- --------------------------------------------------------------------------------------------------------
 
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
+
 -- NODES
-
-
 minetest.register_node("whiteshell:whiteshell", {
-	description = "White shell",
+	description = S("White shell"),
 	drawtype = "normal",
 --	tiles = {"default_desert_sand.png^clams_crushedwhite.png"},
 	tiles = {"default_desert_sand.png"},
