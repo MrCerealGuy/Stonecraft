214f42682a2e00e6fddf225cfda09fe17e1db21b
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-18 12:03:38 +0200

    Added localization to vessels mod

--- games/stonecraft_game/mods/vessels/init.lua
+++ games/stonecraft_game/mods/vessels/init.lua
@@ -1,6 +1,17 @@
 -- Minetest 0.4 mod: vessels
 -- See README.txt for licensing and other information.
 
+--[[
+
+2017-05-18 MrCerealGuy: added intllib support
+
+--]]
+
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 local vessels_shelf_formspec =
 	"size[8,7;]" ..
 	default.gui_bg ..
@@ -33,7 +44,7 @@ local function get_vessels_shelf_formspec(inv)
 end
 
 minetest.register_node("vessels:shelf", {
-	description = "Vessels Shelf",
+	description = S("Vessels Shelf"),
 	tiles = {"default_wood.png", "default_wood.png", "default_wood.png",
 		"default_wood.png", "vessels_shelf.png", "vessels_shelf.png"},
 	paramtype2 = "facedir",
@@ -94,7 +105,7 @@ minetest.register_craft({
 })
 
 minetest.register_node("vessels:glass_bottle", {
-	description = "Glass Bottle (empty)",
+	description = S("Glass Bottle (empty)"),
 	drawtype = "plantlike",
 	tiles = {"vessels_glass_bottle.png"},
 	inventory_image = "vessels_glass_bottle.png",
@@ -120,7 +131,7 @@ minetest.register_craft( {
 })
 
 minetest.register_node("vessels:drinking_glass", {
-	description = "Drinking Glass (empty)",
+	description = S("Drinking Glass (empty)"),
 	drawtype = "plantlike",
 	tiles = {"vessels_drinking_glass.png"},
 	inventory_image = "vessels_drinking_glass_inv.png",
@@ -146,7 +157,7 @@ minetest.register_craft( {
 })
 
 minetest.register_node("vessels:steel_bottle", {
-	description = "Heavy Steel Bottle (empty)",
+	description = S("Heavy Steel Bottle (empty)"),
 	drawtype = "plantlike",
 	tiles = {"vessels_steel_bottle.png"},
 	inventory_image = "vessels_steel_bottle.png",
@@ -175,7 +186,7 @@ minetest.register_craft( {
 -- Glass and steel recycling
 
 minetest.register_craftitem("vessels:glass_fragments", {
-	description = "Pile of Glass Fragments",
+	description = S("Pile of Glass Fragments"),
 	inventory_image = "vessels_glass_fragments.png",
 })
 
