25c9fb80e957e4f89c4e2cfaf0f777a4a5f39f8a
MrCerealGuy <mrcerealguy@gmx.de>
2017-05-27 09:34:40 +0200

    Updated localization

--- games/stonecraft_game/mods/technic/concrete/init.lua
+++ games/stonecraft_game/mods/technic/concrete/init.lua
@@ -6,6 +6,8 @@
 2017-01-06 modified by MrCerealGuy <mrcerealguy@gmx.de>
 	exit if mod is deactivated
 
+2017-05-27 MrCerealGuy: added intllib support
+
 --]]
 
 local DIR_DELIM = DIR_DELIM or "/"
@@ -23,8 +25,9 @@ end
 local technic = rawget(_G, "technic") or {}
 technic.concrete_posts = {}
 
--- Boilerplate to support localized strings if intllib mod is installed.
-local S = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
 
 for i = 0, 31 do
 	minetest.register_alias("technic:concrete_post"..i,
