632dc683513c36028c6a8176a51e36fbee93ec15
MrCerealGuy <mrcerealguy@gmx.de>
2017-06-30 14:04:28 +0200

    Added mod_control.lua

--- builtin/common/mod_control.lua
+++ builtin/common/mod_control.lua
@@ -1,4 +1,23 @@
+--Stonecraft
+--Copyright (C) 2017 MrCerealGuy
+--
+--This program is free software; you can redistribute it and/or modify
+--it under the terms of the GNU Lesser General Public License as published by
+--the Free Software Foundation; either version 2.1 of the License, or
+--(at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Lesser General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public License along
+--with this program; if not, write to the Free Software Foundation, Inc.,
+--51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+--------------------------------------------------------------------------------
+
+-- Gets boolean value from world dialog saved in world.mt
 function core.is_world_option(option)
 	assert(type(option) == "string")
 
@@ -19,6 +38,7 @@ function core.is_world_option(option)
 	return false
 end
 
+-- Checks active world.mt for loading/skipping mod
 function core.skip_mod(mod)
 	assert(type(mod) == "string")
 
