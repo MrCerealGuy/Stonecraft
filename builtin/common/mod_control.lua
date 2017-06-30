456000b02159842899557bb49be5b4b1c02ddadb
MrCerealGuy <mrcerealguy@gmx.de>
2017-06-30 13:47:16 +0200

    Added mod_control.lua

---
+++ builtin/common/mod_control.lua
@@ -0,0 +1,20 @@
+
+function core.is_world_option(option)
+	assert(type(option) == "string")
+
+	local world_conf_option = nil
+
+	local DIR_DELIM = DIR_DELIM or "/"
+	local world_file = core.get_worldpath()..DIR_DELIM.."world.mt"
+	local world_conf = Settings(world_file)
+
+	if world_conf ~= nil then
+		world_conf_option = world_conf:get(option)
+	end 
+
+	if world_conf_option == "true" then
+		return true
+	end
+
+	return false
+end
