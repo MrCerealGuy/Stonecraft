fab7e2d2a61eb5f9b8ca70d50dc80af29248b777
MrCerealGuy <mrcerealguy@gmx.de>
2017-09-18 16:44:05 +0200

    Updated cottages mod

--- games/stonecraft_game/mods/cottages/init.lua
+++ games/stonecraft_game/mods/cottages/init.lua
@@ -56,12 +56,14 @@ cottages.handmill_min_per_turn = 0;
 -- uncomment parts you do not want
 dofile(minetest.get_modpath("cottages").."/nodes_furniture.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_historic.lua");
+dofile(minetest.get_modpath("cottages").."/nodes_feldweg.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_straw.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_anvil.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_doorlike.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_fences.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_roof.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_barrel.lua");
+dofile(minetest.get_modpath("cottages").."/nodes_mining.lua");
 --dofile(minetest.get_modpath("cottages").."/nodes_chests.lua");
 
 -- this is only required and useful if you run versions of the random_buildings mod where the nodes where defined inside that mod
