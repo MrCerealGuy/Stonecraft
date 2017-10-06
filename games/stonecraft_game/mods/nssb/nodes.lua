0b2da2730c4f33bec4ef07e2ed4751bae08e1040
MrCerealGuy <mrcerealguy@gmx.de>
2017-10-06 14:34:32 +0200

    Moved S() function from init.lua to nodes.lua

--- games/stonecraft_game/mods/nssb/nodes.lua
+++ games/stonecraft_game/mods/nssb/nodes.lua
@@ -1,3 +1,13 @@
+--[[
+
+2017-05-18 MrCerealGuy: added intllib support
+
+--]]
+
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
+
 --Materials
 minetest.register_node("nssb:memoryone", {
 	description = S("Memoryone"),
