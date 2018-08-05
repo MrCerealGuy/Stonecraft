53faa29590de85d0e59663016404e04b809b8de1
Andreas Zahnleiter <mrcerealguy@gmx.de>
2018-08-05 10:56:37 +0200

    Revert mingw-std-threads changes

--- src/threading/thread.cpp
+++ src/threading/thread.cpp
@@ -271,8 +271,7 @@ bool Thread::bindToProcessor(unsigned int proc_number)
 
 #elif __MINGW32__
 
-//	return SetThreadAffinityMask(pthread_gethandle(getThreadHandle()), 1 << proc_number);
-	return SetThreadAffinityMask(getThreadHandle(), 1 << proc_number);
+	return SetThreadAffinityMask(pthread_gethandle(getThreadHandle()), 1 << proc_number);
 
 #elif __FreeBSD_version >= 702106 || defined(__linux__)
 
@@ -324,8 +323,7 @@ bool Thread::setPriority(int prio)
 
 #elif __MINGW32__
 
-	//return SetThreadPriority(pthread_gethandle(getThreadHandle()), prio);
-	return SetThreadPriority(getThreadHandle(), prio);
+	return SetThreadPriority(pthread_gethandle(getThreadHandle()), prio);
 
 #else
 
@@ -343,4 +341,3 @@ bool Thread::setPriority(int prio)
 
 #endif
 }
-
