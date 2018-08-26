49032ed81f5df8a2d97359bbb45cb1b0e4a7de0b
MrCerealGuy <mrcerealguy@gmx.de>
2018-08-26 22:38:03 +0200

    Merged areas mod updats

--- games/stonecraft_game/mods/areas/settings.lua
+++ games/stonecraft_game/mods/areas/settings.lua
@@ -6,13 +6,13 @@ local function setting(tp, name, default)
 	local full_name = "areas."..name
 	local value
 	if tp == "boolean" then
-		value = minetest.setting_getbool(full_name)
+		value = minetest.settings:get_bool(full_name)
 	elseif tp == "string" then
-		value = minetest.setting_get(full_name)
+		value = minetest.settings:get(full_name)
 	elseif tp == "position" then
 		value = minetest.setting_get_pos(full_name)
 	elseif tp == "number" then
-		value = tonumber(minetest.setting_get(full_name))
+		value = tonumber(minetest.settings:get(full_name))
 	else
 		error("Invalid setting type!")
 	end
@@ -30,7 +30,7 @@ setting("string", "filename", world_path.."/areas.dat")
 
 -- Allow players with a privilege create their own areas
 -- within the maximum size and number.
-setting("boolean",  "self_protection", true)
+setting("boolean",  "self_protection", true)	-- MERGEINFO: changed by MrCerealGuy
 setting("string",   "self_protection_privilege", "interact")
 setting("position", "self_protection_max_size",      {x=64,  y=128, z=64})
 setting("number",   "self_protection_max_areas",      4)
