ec3b832d0aa9ea296d40ea9b8dd72b1a11d5b981
MrCerealGuy <mrcerealguy@gmx.de>
2018-08-28 11:40:27 +0200

    Added doc/Manual.html generation to linux build process

--- build/build_linux_server.sh
+++ build/build_linux_server.sh
@@ -1,3 +1,5 @@
+stonecraftdir="$( cd .. && pwd)"
+
 dialog --backtitle "Stonecraft Build" --title "Stonecraft Build" --yesno "Start the build process? All previous generated CMake files will be deleted!" 15 60
 antwort=${?}
 
@@ -16,6 +18,14 @@ if [ "$antwort" -eq "0" ]
   then
     ../util/remove_generated_cmakefiles.sh
 fi
+
+# Generate doc/Manual.html from doc/Manual.md with Pandoc
+echo -e "\E[34;47mGenerate Manual.html with Pandoc..."
+
+cd $stonecraftdir/util/
+./convert_manual-md2html.sh
+
+echo -e "\E[34;47mdone!"
 
 cd ../
 cmake . -DBUILD_CLIENT=0 -DBUILD_SERVER=1 -DRUN_IN_PLACE=1 -DENABLE_REDIS=1 -DENABLE_LEVELDB=1
