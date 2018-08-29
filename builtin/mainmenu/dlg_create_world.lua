2d7eca9c1e45364c3723d52323e5e17e9048d15e
MrCerealGuy <mrcerealguy@gmx.de>
2018-08-29 15:35:33 +0200

    Removed legacy darkage mod

--- builtin/mainmenu/dlg_create_world.lua
+++ builtin/mainmenu/dlg_create_world.lua
@@ -389,7 +389,6 @@ local function create_world_buttonhandler(this, fields)
 		["enable_villages"]						=
 		{
 			["enable_villages"]	 				= true,
-			["enable_darkage"]	 				= false,	--explicit deactivated because of backward compatibility for old worlds
 			["enable_mobf_trader"] 				= true,
 			--["enable_moresnow"]	 			= true,
 			["enable_mob_world_interaction"] 	= true
