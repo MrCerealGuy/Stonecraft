fd46176920fb43c497efa3c8f5044c96d806ce5d
MrCerealGuy <mrcerealguy@gmx.de>
2025-04-26 10:32:37 +0200

    26042025-1032

--- build/make_win64_package.bat
+++ build/make_win64_package.bat
@@ -6,6 +6,8 @@ REM Andreas "MrCerealGuy" Zahnleiter <mrcerealguy@gmx.de>
 setlocal
 
 set _dest=..\..\Stonecraft-Release
+set _vers=1.3.2
+set _plat=Win64
 
 mkdir %_dest%
 mkdir %_dest%\bin
@@ -16,17 +18,19 @@ set _what=/E
 
 ROBOCOPY ..\bin                   %_dest%\bin                   *.exe
 ROBOCOPY ..\bin                   %_dest%\bin                   *.dll
-ROBOCOPY ..\builtin               %_dest%\builtin                          %_what%
-ROBOCOPY ..\client                %_dest%\client                           %_what%
-ROBOCOPY ..\clientmods            %_dest%\clientmods                       %_what%
-ROBOCOPY ..\fonts                 %_dest%\fonts                            %_what%
-ROBOCOPY ..\games\stonecraft_game %_dest%\games\stonecraft_game            %_what%
+ROBOCOPY ..\builtin               %_dest%\builtin                                             %_what%
+ROBOCOPY ..\client                %_dest%\client                                              %_what%
+ROBOCOPY ..\clientmods            %_dest%\clientmods                                          %_what%
+ROBOCOPY ..\fonts                 %_dest%\fonts                                               %_what%
+ROBOCOPY ..\games\stonecraft_game %_dest%\games\stonecraft_game                               %_what%
 ROBOCOPY ..\mods                  %_dest%\mods                  *.txt
-ROBOCOPY ..\textures              %_dest%\textures                         %_what%
+ROBOCOPY ..\textures              %_dest%\textures                                            %_what%
 
-ROBOCOPY ..\ %_dest% README.md
-ROBOCOPY ..\ %_dest% LICENSE.txt
-ROBOCOPY ..\ %_dest% stonecraft.conf.example
-ROBOCOPY ..\ %_dest% stonecraft.conf.example.extra
+ROBOCOPY ..\                      %_dest%                       README.md
+ROBOCOPY ..\                      %_dest%                       LICENSE.txt
+ROBOCOPY ..\                      %_dest%                       stonecraft.conf.example
+ROBOCOPY ..\                      %_dest%                       stonecraft.conf.example.extra
+
+7z a -tzip Stonecraft-%_vers%-%_plat%.zip -r %_dest%\*
 
 pause
